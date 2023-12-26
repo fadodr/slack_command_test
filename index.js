2a0f933c1f78fac542aa0cb637eaa946436f8a3a
agboola fuad <fagboola09@gmail.com>
2023-12-26 17:53:58 +0100

    string to json

--- index.js
+++ index.js
@@ -3,28 +3,35 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 try {
+  const app = new App({
+    signingSecret: process.env.SECRET,
+    token: process.env.TOKEN,
+  });
 
-const app = new App({
-  signingSecret: process.env.SECRET,
-  token: process.env.TOKEN,
-});
+  app.command('/fado', async ({ command, ack, say }) => {
+    await ack();
 
-    app.command('/fado', async ({ command, ack, say }) => {
-    
-        console.log(`Received a command: ${command.text}`);
-  // Acknowledge the command request
-  await ack();
+    console.log(`Received a command: ${command.text}`);
+    // Acknowledge the command request
 
-  // Respond to the command
-  await say(`Received a command: ${command.text}`);
-});
+    // Respond to the command
+    await say({
+      status: true,
+      data: {
+        user: {
+          firstname: 'fado',
+          lastname: 'agboola',
+        },
+      },
+    });
+  });
 
-(async () => {
+  (async () => {
     // Start the app
     await app.start(process.env.PORT || 3000);
 
     console.log('⚡️ Bolt app is running!');
-})();
+  })();
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
